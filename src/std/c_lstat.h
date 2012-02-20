7285cfd21f6d5bfee6a199f0d9a30904879ced9e
Dominik Schmidt <dev@dominik-schmidt.de>
2012-02-20 16:10:50 +0100

    Add missing c_lstat.h file

---
+++ src/std/c_lstat.h
@@ -0,0 +1,32 @@
+/*
+ * c_lstat - define lstat if not available on current platform
+ *
+ * Copyright (c) 2012      by Dominik Schmidt <dev@dominik-schmidt.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * vim: ft=c.doxygen ts=2 sw=2 et cindent
+ */
+
+#ifndef _C_LSTAT_H
+#define _C_LSTAT_H
+
+#include <sys/stat.h>
+
+#ifndef HAVE_LSTAT
+#define lstat stat
+#endif
+
+#endif /* _C_LSTAT_H */
